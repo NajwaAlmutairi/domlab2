e65d2abbed2f44b79807c3fdd5449b82fc7866c8
najwa <NajwaBuayjan@gmail.com>
2024-09-11 14:24:45 +0300

    solving the lab

---
+++ main.js
@@ -0,0 +1,28 @@
+
+function changeWidthHeight() {
+    let boxDiv = document.getElementById('box');
+    let randomWidth = Math.floor(Math.random() * (500 - 100 + 1)) + 100;
+    let randomHeight = Math.floor(Math.random() * (500 - 100 + 1)) + 100;
+
+    boxDiv.style.width = randomWidth + 'px';
+    boxDiv.style.height = randomHeight + 'px';
+
+    const colorsList = [
+        'aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 'lime', 'maroon', 'navy',
+        'olive', 'purple', 'red', 'silver', 'teal', 'white', 'yellow',
+        'aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque',
+        'blanchedalmond', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse',
+        'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan',
+        'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkgrey',
+        'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid',
+        'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray',
+        'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue',
+        'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen'
+    ];
+    let randomIndex = Math.floor(Math.random() * 63);
+    let selectedColor = colorsList[randomIndex]
+    boxDiv.style.backgroundColor = selectedColor;
+
+}
+
+setInterval(changeWidthHeight, 2000);
